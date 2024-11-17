e890b1c9af9fdfab055800c136420a9b47b7998c
anyanswer1125 <kll5893@gmail.com>
2024-11-18 03:36:20 +0900

    회원가입 페이지 제작 완료 및 메인 페이지 작업중

---
+++ JS/register.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector('.loginBtn');
+
+const IdInput = document.querySelector('.id') 
+const PwdInput = document.querySelector('.pwd') 
+const nameInput = document.querySelector('.name');
+const usernameInput = document.querySelector('.username');
+
+function BtnColorChange() {
+  // 아이디, 비밀번호란에 입력한 값이 조건에 일치하면 버튼 색상 변경 및, 클릭 가능하게 만들어주는 함수
+  const IdText = document.querySelector('.id').value;
+  const PwdText = document.querySelector('.pwd').value;
+  const nameText = document.querySelector('.name').value;
+  const usernameText = document.querySelector('.username').value;
+
+  if (IdText.includes('@') && 
+  IdText.includes(".com") &&
+  nameText.length >= 1 &&
+  usernameText.length >= 1 &&
+  PwdText.length >= 4){
+    btn.style.background = "rgb(0, 149, 246, 100)";
+    btn.style.cursor = "pointer";
+  }else{
+    btn.style.background = "rgb( 103, 181, 250)";
+  }
+};
+
+IdInput.addEventListener('input', BtnColorChange);
+PwdInput.addEventListener('input', BtnColorChange);
+nameInput.addEventListener('input', BtnColorChange)
+usernameInput.addEventListener('input', BtnColorChange)
